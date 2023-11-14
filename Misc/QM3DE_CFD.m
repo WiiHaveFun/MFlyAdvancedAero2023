e1e2660a76827732b813eb489f210df320cc324b
michaelchen <jr.michael.chen@gmail.com>
2023-11-14 16:31:02 -0500

    PADA CFD analysis

--- Misc/QM3DE_CFD.m
+++ Misc/QM3DE_CFD.m
@@ -1,6 +1,6 @@
 %% Aircraft Parameters
 
-b = 28 / 39.37; % m
+b = 28 / 39.37; % in to m
 S = 0.1043; % m^2
 AR = b^2 / S;
 
@@ -56,6 +56,68 @@ plot(CD_CFD_scaled, CL_CFD, "LineWidth", 3);
 plot(CD_AVL, CL_AVL, "LineWidth", 3);
 scatter(CD_windtunnel, CL_windtunnel, "filled");
 
-xlabel("CD");
-ylabel("CL");
-legend("CFD", "CFD Scaled", "AVL", "Wind Tunnel");
+title("PADA Drag Polars")
+xlabel("C_{D}", "FontSize", 14);
+ylabel("C_{L}", "FontSize", 14);
+legend("CFD", "CFD Scaled", "AVL", "Wind Tunnel Data", Location="southeast");
+xlim([0,0.5])
+set(gca, "FontSize", 14);
+
+figure(3);
+plot(CD_CFD_scaled, CL_CFD, "LineWidth", 3);
+title("PADA Drag Polar")
+xlabel("C_{D}");
+ylabel("C_{L}");
+xlim([0,0.5])
+set(gca, "FontSize", 14);
+
+%% Save scale polar to CSV
+output = round([alpha_CFD, CL_CFD, CD_CFD_scaled], 4);
+T = array2table(output);
+T.Properties.VariableNames = ["alpha", "CL", "CD"];
+writetable(T, 'QM-3D_drag_polar_scaled.csv');
+
+%% [Extra stuff]
+%% Thrust curve
+
+throttle = 1.0; % Probably not very accurate
+T = @(v) (3.63 - 0.0694 * v - 5.11e-4 * v.^2) * throttle; % Thrust in N, velcity in m/s
+
+%% Flight Test Parameters
+
+W = 16; % ozf
+
+p = 29.34 * 3386.39; % Station pressure in inHg converted to Pa. Barometric pressure of 30.10 inHg
+Temp = (46 - 32) * 5 / 9 + 273.15; % Temperature in deg F to K
+
+rho = p / (287 * Temp);
+
+%% Cruise velocity prediction
+
+[ppCruiseVel, ppMTOW] = findMTOWCruiseVel(alpha_CFD, CL_CFD, CD_CFD_scaled, T, rho, S);
+
+a = linspace(-5, 15, 50);
+
+figure(2);
+yyaxis left
+plot(a, ppval(ppCruiseVel, a), LineWidth=2);
+hold on;
+
+xlabel("Angle of attack (deg)");
+ylabel("Cruise velocity (m/s)");
+
+yyaxis right
+plot(a, ppval(ppMTOW, a) * 35.274, LineWidth=2);
+ylabel("MTOW (oz)")
+
+yline(W);
+
+%% Calculate cruise velocity
+
+cruise_alpha = fzero(@(a) ppval(ppMTOW, a) * 35.274 - W, 0);
+cruise_velocity = ppval(ppCruiseVel, cruise_alpha);
+
+disp("Cruise angle of attack (deg)");
+disp(cruise_alpha);
+disp("Cruise velocity (m/s)");
+disp(cruise_velocity);
